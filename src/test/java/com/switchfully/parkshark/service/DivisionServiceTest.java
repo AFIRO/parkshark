860d6398e5956f77ac9d1d5c1472203efe23a33e
Sander Janssens <sander.janssens@outlook.be>
2021-11-18 16:17:23 +0100

    Changes by freeh

--- src/test/java/com/switchfully/parkshark/service/DivisionServiceTest.java
+++ src/test/java/com/switchfully/parkshark/service/DivisionServiceTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-@SpringBootTest
+//@SpringBootTest
 class DivisionServiceTest {
 
     private DivisionService divisionService;
