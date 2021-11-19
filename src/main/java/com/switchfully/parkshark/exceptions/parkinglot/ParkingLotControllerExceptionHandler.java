bc028792d870de7b7ef18c8f1370d56e0a4ed7f9
Christoph <christoph.parrez@gmail.com>
2021-11-19 15:45:16 +0100

    Optimized Imports

--- src/main/java/com/switchfully/parkshark/exceptions/parkinglot/ParkingLotControllerExceptionHandler.java
+++ src/main/java/com/switchfully/parkshark/exceptions/parkinglot/ParkingLotControllerExceptionHandler.java
@@ -1,9 +1,5 @@
 package com.switchfully.parkshark.exceptions.parkinglot;
 
-import com.switchfully.parkshark.exceptions.member.BadCreateAddressException;
-import com.switchfully.parkshark.exceptions.member.BadCreateLicensePlateException;
-import com.switchfully.parkshark.exceptions.member.BadCreateMemberException;
-import com.switchfully.parkshark.exceptions.member.NoSuchMemberException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
