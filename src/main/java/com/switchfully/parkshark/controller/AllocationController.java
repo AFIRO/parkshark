c951c1cbcd9d11501075a380e6d48c8daeadc9c3
Christoph <christoph.parrez@gmail.com>
2021-11-22 09:50:46 +0100

    Changed the order of params in getAllAllocations

--- src/main/java/com/switchfully/parkshark/controller/AllocationController.java
+++ src/main/java/com/switchfully/parkshark/controller/AllocationController.java
@@ -5,9 +5,11 @@ import com.switchfully.parkshark.dto.allocation.CreateAllocationDTO;
 import com.switchfully.parkshark.entity.Allocation;
 import com.switchfully.parkshark.service.AllocationService;
 import com.switchfully.parkshark.switchsecure.SecurityGuard;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,11 +30,11 @@ public class AllocationController {
     @GetMapping(produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     @SecurityGuard(SecurityGuard.ApiUserRole.MANAGER)
-    public List<AllocationDTO> getAllAllocations(@RequestParam(required = false) Allocation.AllocationStatus filterStatus,
-                                                 @RequestParam(required = false) Integer maxResults,
-                                                 @RequestParam Allocation.AllocationSorter sort) {
+    public List<AllocationDTO> getAllAllocations(@RequestParam(required = false) Allocation.AllocationStatus statusFilter,
+                                                 @RequestParam() Allocation.AllocationSorter sortByStartHour,
+                                                 @RequestParam(required = false) Integer maxResults) {
         logger.info("Attempting to get all allocations.");
-        return allocationService.getAllAllocations(filterStatus, maxResults, sort);
+        return allocationService.getAllAllocations(statusFilter, maxResults, sortByStartHour);
     }
 
     @PostMapping(consumes = "application/json", produces = "application/json")
